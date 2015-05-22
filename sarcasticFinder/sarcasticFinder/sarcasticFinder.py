3398046e81bb4acfa5bc0bd95663a0e227d3e638
keil <keilbroing@hotmail.com>
2015-05-22 00:22:04 -0700

    removed empty case special seems to work

--- sarcasticFinder/sarcasticFinder/sarcasticFinder.py
+++ sarcasticFinder/sarcasticFinder/sarcasticFinder.py
@@ -76,10 +76,11 @@ for l in reviews:
     #time.sleep(3)
 
 
-
+sarcastic_answer = []
 
 LabelCnt1 = 0
 for l in reviews:
+    sarcastic_answer.append(l[0])
     if l[0] == "1":
         LabelCnt1 = LabelCnt1 + 1
 print "number of sarcastic 1's =" + str(LabelCnt1) 
@@ -96,12 +97,13 @@ P0 = float(LabelCnt0) / (float(LabelCnt0) + float(LabelCnt1))
 
 #input_string = "just washed my sisters car, got totally soaked coz the actual hose thing came off, but i did a good job i guess."
 
+sarcastic_guess = []
 for input_string in reviews:
-
+    print input_string
     #Check aP(when class = 1)
     PC1 = [float(P1)]
     for u in unqiueWords:
-        probability = []
+        probability = 0 
         cnt = 0
         #input contains keyword
         if(re.search(r'\b' + u + r'\b' ,input_string)):
@@ -110,7 +112,8 @@ for input_string in reviews:
                     if(re.search(r'\b' + u + r'\b' ,l)):
                         cnt = cnt + 1
             #print "key found=" + u + "\t\t" + str(cnt)
-            probability.append((float(cnt) + 1) / (float(LabelCnt1) + float(len(unqiueWords))  ))
+            #probability = float(cnt) + 1) / (float(LabelCnt1) + float(len(unqiueWords))  
+            probability = float(cnt)  / (float(LabelCnt1) )  
             #print "prob=" + str(probability)
        #input does not contain keyword so count # of reviews that dont have keyword
         else:
@@ -119,16 +122,17 @@ for input_string in reviews:
                     if(not re.search(r'\b' + u + r'\b' ,l)):
                         cnt = cnt + 1
             #print "key not found=" + u + "\t\t" + str(cnt)
-            probability.append((float(cnt) + 1) / (float(LabelCnt1) + float(len(unqiueWords))  ))
+            #probability = float(cnt) + 1) / (float(LabelCnt1) + float(len(unqiueWords))  
+            probability = float(cnt)  / (float(LabelCnt1) )  
             #print "prob=" + str(probability)
-        PC1.append( functools.reduce(operator.mul,probability))
+        PC1.append( probability)
         #print PC1
 
     #Check aP(when class = 0)
     PC0 = [float(P0)]
     for u in unqiueWords:
         cnt = 0
-        probability = []
+        probability = 0 
         #input contains keyword
         if(re.search(r'\b' + u + r'\b' ,input_string)):
             for l in reviews: 
@@ -136,7 +140,8 @@ for input_string in reviews:
                     if(re.search(r'\b' + u + r'\b' ,l)):
                         cnt = cnt + 1
             #print "key found=" + u + "\t\t" + str(cnt)
-            probability.append((float(cnt) + 1) / (float(LabelCnt0) + float(len(unqiueWords))  ))
+            #probability = ((float(cnt) + 1) / (float(LabelCnt0) + float(len(unqiueWords))  ))
+            probability = float(cnt)  / (float(LabelCnt0) )  
             #print "prob=" + str(probability)
        #input does not contain keyword so count # of reviews that dont have keyword
         else:
@@ -145,15 +150,33 @@ for input_string in reviews:
                     if(not re.search(r'\b' + u + r'\b' ,l)):
                         cnt = cnt + 1
             #print "key not found=" + u + "\t\t" + str(cnt)
-            probability.append((float(cnt) + 1) / (float(LabelCnt0) + float(len(unqiueWords))  ))
+            #probability = ((float(cnt) + 1) / (float(LabelCnt0) + float(len(unqiueWords))  ))
+            probability = float(cnt)  / (float(LabelCnt0) )  
             #print "prob=" + str(probability)
-        PC0.append( functools.reduce(operator.mul,probability))
+        PC0.append( probability)
         #print PC0
 
 
     print "PC0 = " + str(functools.reduce(operator.mul,PC0))
     print "PC1 = " + str(functools.reduce(operator.mul,PC1))
     if( functools.reduce(operator.mul,PC0) > functools.reduce(operator.mul,PC1)):
+        sarcastic_guess.append("0")
         print "PC0 was bigger"
     else:
+        sarcastic_guess.append("1")
         print "PC1 was bigger!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    #break
+
+rightCnt = 0
+wrongCnt = 0
+for x in range(0, len(sarcastic_answer)):
+    if(sarcastic_answer[x] == sarcastic_guess[x]):
+        rightCnt = rightCnt + 1
+        print "you guessed right"
+    else:
+        wrongCnt = wrongCnt + 1
+        print "wrong"
+
+print "right Cnt " + str(rightCnt)
+print "wrong Cnt " + str(wrongCnt)
+print "test"
